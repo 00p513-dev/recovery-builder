4b2884ae2bbde2c7dfb36d5985c8913983aef510
Amy <amy@blisslabs.org>
2021-08-16 17:50:43 +0100

    New codename: spinel

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
-lunch omni_Infinix-X627STU-userdebug
+lunch omni_spinel-userdebug
 #mkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc/ld.config.txt
 make -j$(nproc) recoveryimage
