a62acf8e55c17f78e7f03456d0c1242c9b46d7a1
Amy <amy@blisslabs.org>
2021-08-16 17:09:07 +0100

    Infinix-X627STU

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 lunch omni_$DEVICE-userdebug
-mkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
-touch /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc/ld.config.txt
+#mkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
+#touch /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc/ld.config.txt
 make -j$(nproc) recoveryimage
